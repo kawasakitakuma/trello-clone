a515f0c41948ef43c1f2bc743bf7f21afab0145c
kawasakitakuma <takumahuniv21@gmail.com>
2024-09-09 23:24:15 +0900

    背景画像適用

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from "./components/header/Header";
 import TaskCards from "./components/task/TaskCards";
+import mountainImage from "./images/mountain.jpg";
 
 function App() {
   return (
-    <div>
+    <div className="app" style={{ backgroundImage: `url(${mountainImage})` }}>
       <Header />
       <TaskCards />
     </div>
